d754a9a10a351f0071dffdace629aa64198cb600
Nguyen Van Tam <tamnvhustcc@gmail.com>
2025-02-12 17:03:20 +0700

    added ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -24,6 +25,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-HN6KKE6PGE" />
     </html>
   );
 }
